26c510f1964f0b535c0136d4c5a535f7a0cdd02a
Brian Fontana <brian.fontana@allscripts.com>
2023-09-10 22:12:51 -0400

    housekeeping

--- HttpTriggerDemo/HttpTriggerExample.cs
+++ HttpTriggerDemo/HttpTriggerExample.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using System.Net;
-using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
