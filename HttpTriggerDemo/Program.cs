2eae2513e27335b33dba28a5e47d09e0eee656e3
Brian Fontana <brian@codefoxtrot.com>
2026-02-09 00:23:49 -0500

    Upgrade nuget packages
    Seems the latest Microsoft.ApplicationInsights.WorkerService no longer requires explicit logging configuration as now filters apply to all registered providers.

--- HttpTriggerDemo/Program.cs
+++ HttpTriggerDemo/Program.cs
@@ -1,14 +1,8 @@
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.ApplicationInsights;
 
-var host = new HostBuilder()
+IHost host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
-    {
-        services.AddApplicationInsightsTelemetryWorkerService();
-    })
     .ConfigureLogging(logging =>
     {
         logging.AddConsole();
@@ -18,13 +12,6 @@ var host = new HostBuilder()
         logging.AddFilter("Azure", LogLevel.Warning);
         logging.AddFilter("Microsoft.Extensions.Diagnostics", LogLevel.Warning);
         logging.AddFilter("HttpTriggerDemo", LogLevel.Trace);
-
-        logging.AddFilter<ApplicationInsightsLoggerProvider>("Host.Results", LogLevel.Error);
-        logging.AddFilter<ApplicationInsightsLoggerProvider>("Host.Aggregator", LogLevel.Trace);
-        logging.AddFilter<ApplicationInsightsLoggerProvider>("Function", LogLevel.Error);
-        logging.AddFilter<ApplicationInsightsLoggerProvider>("Azure", LogLevel.Warning);
-        logging.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft.Extensions.Diagnostics", LogLevel.Warning);
-        logging.AddFilter<ApplicationInsightsLoggerProvider>("HttpTriggerDemo", LogLevel.Trace);
     })
     .Build();
 
